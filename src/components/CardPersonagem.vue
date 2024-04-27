7ccf33f01a0faeb8d646ee11bb2d2c2df8452e52
kawamotomarcela <kawamotomarcela@gmail.com>
2024-04-27 13:07:17 -0300

    subindo projeto

---
+++ src/components/CardPersonagem.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+const personagem = defineProps(["id_img", "name", "status", "species", "type", "location"]);
+const image_url = "https://rickandmortyapi.com/api/character/avatar/" + personagem.id_img + ".jpeg"
+console.log(personagem);
+</script>
+<template>
+
+    <div class="card m-3" style="width: 18rem;">
+        <img :src="image_url" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ personagem.name }}</h5>
+            <div class="d-flex">
+                <span>Status: </span>
+                <p class="card-text">{{ personagem.status }}</p>
+            </div>
+            <div class="d-flex">
+                <span>Especie: </span>
+                <p class="card-text">{{ personagem.species }}</p>
+            </div>
+            <div class="d-flex">
+                <span>Genero: </span>
+                <p class="card-text">{{ personagem.type }}</p>
+            </div>
+            <div class="d-flex">
+                <span>Localização: </span>
+                <p class="card-text">{{ personagem.location }}</p>
+            </div>
+        </div>
+    </div>
+
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted, reactive, ref } from 'vue';
+import CardPersonagem from '../components/CardPersonagem.vue'
+
+let personagens = reactive(ref());
+
+onMounted(() => {
+
+  fetch("https://rickandmortyapi.com/api/character")
+    .then(response => response.json())
+    .then(response => {
+      personagens.value = response.results;
+    })
+})
+</script>
+
+<template>
+  <CardPersonagem v-for="personagem in personagens" 
+    :id_img="personagem.id"
+    :name="personagem.name"
+    :status="personagem.status"
+    :species="personagem.species"
+    :type="personagem.type"
+    :location="personagem.location.name"
+  />
+</template>
